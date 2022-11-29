d4161a444885efb00779359b9c2415c599db30ca
Алексей Макаров <mak-ov@mail.ru>
2022-11-29 12:06:40 +0300

    Добавлены файлы Messages Settings, Friends - запол

---
+++ src/state.jsx
@@ -0,0 +1,8 @@
+const user = {
+  name: "Иван",
+  lastname: "Иванов",
+  email: "Ivan@ya.ru",
+  id: 2,
+  about: "Тут я рассказываю о себе",
+  avatar: "https://www.seekpng.com/png/full/356-3562377_personal-user.png",
+};
